2990fe39156bd581802c8d54c323a05d2f618088
Jack Alvarez <jalvarezm1995@outlook.com>
2023-05-24 13:12:24 -0400

    Update tradinggym.py

--- tradinggym.py
+++ tradinggym.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 class CryptoEnvironment(Env):
-    def __init__(self, initial_value=10000, observations=None, max_steps=100, random_split=True, window_size=5, trade_fee=0.0045, slippage = 0.005, order_fraction = 0.2, reward_function = None, price_column = 'Close'):
+    def __init__(self, initial_value=10000, observations=None, max_steps=100, random_split=True, window_size=5, trade_fee=0.0045, slippage = 0.005, order_fraction = 0.2, reward_function = None, price_column = 'Close', hold=False, diversity_penalty=0.001):
         """
         Initializes the CryptoEnvironment object.
 
@@ -22,6 +22,8 @@ class CryptoEnvironment(Env):
             order_fraction (float): Ratio of maximum possible order to execute.
             reward_function (function): Custom reward function.
             price_column (str): Name of the column containing price data.
+            hold (bool): Include Hold in the action space.
+            diversity_penalty (float): Reduces reward gaming by penalizing agents who mostly hold their position.
 
         Raises:
             ValueError: If observation data is missing or not longer than double the window size.
@@ -47,7 +49,10 @@ class CryptoEnvironment(Env):
         self.order_fraction = order_fraction  # Ratio of maximum possible order to execute  
         self.price_column = price_column # Name of the column containing price data
         self.rewards = list([i for i in range(window_size)])
-
+        self.action_history = list([0 for i in range(window_size)])
+        self.hold = hold # Whether to include hold in the action space
+        self.diversity_penalty = diversity_penalty # Reduces reward gaming by penalizing agents who mostly hold their position
+     
         if self.observations is None:
             raise ValueError("Observation data is missing.")
 
@@ -57,7 +62,10 @@ class CryptoEnvironment(Env):
 
         # Define the action and observation spaces
         num_columns = self.observations.shape[1] - 1  # Exclude the price column
-        self.action_space = spaces.Discrete(3)  # 0: Buy, 1: Sell, 2: Hold
+        if self.hold:
+            self.action_space = spaces.Discrete(3) 
+        else:
+            self.action_space = spaces.Discrete(2) 
         self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.window_size, num_columns), dtype=np.float32)
 
         # User may supply a custom reward function
@@ -142,6 +150,9 @@ class CryptoEnvironment(Env):
             tuple: Tuple containing the next observation, reward, done flag, and additional info.
         """
 
+        self.action_history.pop(0)
+        self.action_history.append(action)
+
         current_price = self.observations.iloc[self.current_step][self.price_column]
         total_fees = self.trade_fee + np.random.uniform(-self.slippage, self.slippage)
         if action == 0:  # Buy
@@ -182,33 +193,44 @@ class CryptoEnvironment(Env):
 
             ### TODO: add balance and shares to observations
 
+    def _get_reward(self, action):
+        time_shift = 1
+        prev_price = self.observations.iloc[self.current_step - 2 + time_shift][self.price_column]
+        current_price = self.observations.iloc[self.current_step - 1 + time_shift][self.price_column]
 
-    def _get_reward(self,action):
-        """
-        Calculates the reward for the given action.
+        prev_portfolio_value = self.calculate_portfolio_value(current_price=prev_price)
+        current_portfolio_value = self.calculate_portfolio_value(current_price=current_price)
 
-        Args:
-            action (int): Action for which to calculate the reward.
+        # Calculate immediate profit/loss from the current action
+        if action == 0:  # Buy
+            immediate_profit = (current_portfolio_value - prev_portfolio_value) / prev_portfolio_value
+        elif action == 1:  # Sell
+            immediate_profit = (prev_portfolio_value - current_portfolio_value) / current_portfolio_value
+        else:  # Hold
+            immediate_profit = 0
 
-        Returns:
-            float: Reward value.
-        """
+        # Calculate diversity penalty
+        diversity_penalty = 0.0
+        window_size = self.window_size  # Define the window size
 
-        total_fees = self.trade_fee + np.random.uniform(-self.slippage, self.slippage)
-        current_price = self.observations.iloc[self.current_step][self.price_column]
-        prev_price = self.observations.iloc[self.current_step - 1][self.price_column]
-        prev_portfolio_value = self.balance + (((1 - total_fees)* prev_price) * self.shares)
-        current_portfolio_value = self.balance + (((1 - total_fees)* current_price) * self.shares)
+        # Track the frequency or distribution of actions over the sliding window
+        action_history = self.action_history[-window_size:]
+        action_counts = np.bincount(action_history)
+        action_probabilities = action_counts / window_size
 
-        n1_price = self.observations.iloc[self.current_step+1][self.price_column]
-        n2_price = self.observations.iloc[self.current_step+2][self.price_column]
+        valid_probabilities = action_probabilities[action_probabilities != 0]
+        entropy = -np.sum(valid_probabilities * np.log(valid_probabilities)) if len(valid_probabilities) > 0 else 0
 
-        n1_portfolio_value = self.balance + (n1_price * self.shares)
-        n2_portfolio_value = self.balance + (n2_price * self.shares)
+        # Apply penalty based on entropy (penalize low entropy)
+        diversity_penalty = self.diversity_penalty * (1 - entropy)  # Adjust penalty value as needed
 
-        percent_diff = (1000*(current_portfolio_value - prev_portfolio_value) / prev_portfolio_value)
+        reward = immediate_profit - diversity_penalty
 
-        return percent_diff 
+        return reward
+
+
+
+
 
     def calculate_portfolio_value(self, current_price):
         shares_value = current_price*self.shares
@@ -316,7 +338,7 @@ class CryptoEnvironment(Env):
                     axs[0].scatter(index, current_price, color='green', marker='o', s=marker_size)
                 elif action == 1:  # Sell
                     axs[0].scatter(index, current_price, color='red', marker='o', s=marker_size)
-                else: axs[0].scatter(index, current_price, color='blue', marker='o', s=marker_size)
+                elif action==2: axs[0].scatter(index, current_price, color='blue', marker='o', s=marker_size)
 
             # Update the observation for the next step
             observation = next_observation
